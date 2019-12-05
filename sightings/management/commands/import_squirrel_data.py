80bd0d5aced4ca60529afab4b1a0d56b77026da8
Mavis Ming <xm2234@columbia.edu>
2019-12-05 20:41:24 +0000

    realize importing

--- sightings/management/commands/import_squirrel_data.py
+++ sightings/management/commands/import_squirrel_data.py
@@ -5,6 +5,9 @@ from django.core.management.base import BaseCommand
 from sightings .models import Squirrel
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('csv_file')
+
     def handle(self, *args, **options):
         with open(options['csv_file']) as fp:
             reader = csv.DictReader(fp)
