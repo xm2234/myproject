e5036f4c6e704a9faae0f9557683943bd94f6fe4
Mavis Ming <xm2234@columbia.edu>
2019-12-06 00:39:50 +0000

    add urls

--- sightings/urls.py
+++ sightings/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('map/',views.map),
+    # path('/sightings/<unique-squirrel-id>', views.update),
 
 ]
--- sightings/views.py
+++ sightings/views.py
@@ -5,30 +5,27 @@ from django.shortcuts import redirect
 from .models import Squirrel
 from .forms import SquirrelForm
 
-def all_squirrel(request):
+def map(request):
     squirrels = Squirrel.objects.all()
     context = {
             'squirrels':squirrels,
     }
-    return render(request, 'sightings/all.html',context)
+    return render(request, 'sightings/map.html',context)
 
-def squirrel_details(request, Unique_Squirrel_ID):
-    squirrel = Squirrel.objects.get(id=Unique_Squirrel_ID)
-    return HttpResponse(pet.name)
-def edit_pet(request, pet_id):
-    pet = Pet.objects.get(id=pet_id)
+def update(request, Unique_Squirrel_ID):
+    squirrel = Squirrel.objects.filter(id=Unique_Squirrel_ID)[0]
     if request.method == 'Post':
         # check form data
-        form = PetForm(request.POST, instance=pet)
+        form = SquirrelForm(request.POST, instance=squirrel)
         if form.is_valid():
             form.save()
-            return redirect(f'/adopt/{pet_id}')
+            return redirect(f'/sightings/{Unique_Squirrel_ID}')
     else:
-        form = PetForm()
+        form = SquirrelForm()
     context = {
         'form': form,
     }
-    return render(request, 'adopt/edit.html', context)
+    return render(request, 'sightings/edit.html', context)
 
 def add_squirrel(request):
     if request.method == 'POST':
